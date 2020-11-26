0cea8b617ac10852313a0ac67fd2bb0074bdb3e0
macloujulian <65403593+macloujulian@users.noreply.github.com>
2020-11-26 18:42:44 -0300

    Update parametrizadoDSL.groovy

--- parametrizadoDSL.groovy
+++ parametrizadoDSL.groovy
@@ -1,40 +1,40 @@
 job('ejemplo2-job-DSL') {
 	description('Job DSL de ejemplo para el curso de Jenkins')
   	scm {
-      git('https://github.com/macloujulian/jenkins.job.parametrizado.git', 'main') { node ->
-        node / gitConfigName('macloujulian')
-        node / gitConfigEmail('macloujulian@gmail.com')
+      		git('https://github.com/macloujulian/jenkins.job.parametrizado.git', 'main') { node ->
+        		node / gitConfigName('macloujulian')
+        		node / gitConfigEmail('macloujulian@gmail.com')
       }
     } 
   	parameters {
    		stringParam('nombre', defaultValue = 'Julian', description = 'Parametro de cadena para el Job Booleano')
-      	choiceParam('planeta', ['Mercurio', 'Venus', 'Tierrra', 'Marte', 'Jupiter', 'Saturno', 'Urano', 'Neptuno'])
-      	booleanParam('agente', false)
+      		choiceParam('planeta', ['Mercurio', 'Venus', 'Tierrra', 'Marte', 'Jupiter', 'Saturno', 'Urano', 'Neptuno'])
+      		booleanParam('agente', false)
     }
   	triggers {
-    	cron('H/7 * * * *')
+    		cron('H/7 * * * *')
     }
   	steps {
-    	shell("bash jobscript.sh")
+    		shell("bash jobscript.sh")
     }
   	publishers {
-      	mailer('macloujulian@gmail.com', true, true)
-      	slackNotifier {
-          notifyAborted(true)
-          notifyEveryFailure(true)
-          notifyNotBuilt(false)
-          notifyUnstable(false)
-          notifyBackToNormal(true)
-          notifySuccess(false)
-          notifyRepeatedFailure(false)
-          startNotification(false)
-          includeTestSummary(false)
-          includeCustomMessage(false)
-          customMessage(null)
-          sendAs(null)
-          commitInfoChoice('NONE')
-          teamDomain(null)
-          authToken(null)
+      		mailer('macloujulian@gmail.com', true, true)
+      		slackNotifier {
+		  notifyAborted(true)
+		  notifyEveryFailure(true)
+		  notifyNotBuilt(false)
+		  notifyUnstable(false)
+		  notifyBackToNormal(true)
+		  notifySuccess(false)
+		  notifyRepeatedFailure(false)
+		  startNotification(false)
+		  includeTestSummary(false)
+		  includeCustomMessage(false)
+		  customMessage(null)
+		  sendAs(null)
+		  commitInfoChoice('NONE')
+		  teamDomain(null)
+		  authToken(null)
         }
     }
 }
