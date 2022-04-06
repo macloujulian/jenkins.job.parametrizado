888ec3ab1ccf9e70ed02e0c11f45050337da13c2
IsraelGalvez <78826794+IsraelGalvez@users.noreply.github.com>
2022-04-05 18:11:56 -0700

    Create paramtrizadoDSL.groovy

---
+++ paramtrizadoDSL.groovy
@@ -0,0 +1,40 @@
+job('ejemplo3-job-DSL'){
+  description('Job DSL de ejemplo para el curso de Jenkins')
+  scm {
+  	git('https://github.com/macloujulian/jenkins.job.parametrizado.git', 'main') { node ->
+    	node / gitConfigName('IsraelGalvez')
+        node / gitConfigEmail('galvez.rli.51@gmail.com')
+    }
+  }
+  parameters {
+    stringParam('nombre', defaultValue = 'Jualian', description = 'Parametro de cadena para el Job Booleano')
+    choiceParam('planeta' , ['Mercurio', 'Venus', 'Tierra', 'Marte', 'Jupiter', 'Saturno', 'Urano', 'Neptuno'])
+    booleanParam('agente', false)
+  }
+  triggers {
+    cron( 'H/7**')
+  }
+  steps{
+  	shell("bash jobscript.sh")
+  }
+  publishers {
+  	mailer('nakamakey@gmail.com', true, true)
+  slackNotifier {
+    notifyAborted(true)
+    notifyEveryFailure(true)
+    notifyNotBuilt(false)
+    notifyUnstable(false)
+    notifyBackToNormal(true)
+    notifySuccess(false)
+    notifyRepeatedFailure(false)
+    startNotification(false)
+    includeTestSummary(false)
+    includeCustomMessage(false)
+    customMessage(null)
+    sendAs(null)
+    commitInfoChoice('NONE')
+    teamDomain(null)
+    authToken(null)
+   }
+  }
+}
